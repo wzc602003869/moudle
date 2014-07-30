53683ceb000e7bdc0f75bd02695150762b23782d
wzc602003869 <602003869@qq.com>
2014-07-30 15:42:26 +0800

    完善了canvas涂抹功能，已经可以在50%之后将canvas隐藏

--- js/canvas.js
+++ js/canvas.js
@@ -28,13 +28,31 @@ $("canvas").on("touchstart",function(e){
         x = x1;
         y = y1;
         ctx.stroke();
-        $(document).on("touchend",function(){
-            ctx.closePath();
-            ctx.beginPath();
-            ctx.arc(x,y,25,0,Math.PI*2);
-            ctx.fill();
-            ctx.closePath();
-        })
+    })
+    $(document).on("touchend",function(){
+        ctx.closePath();
+        ctx.beginPath();
+        ctx.arc(x,y,25,0,Math.PI*2);
+        ctx.fill();
+        ctx.closePath();
+        var imgData = ctx.getImageData(0, 0,width,height),
+            pixles = imgData.data,
+            transPixs = [];
+        for (var i = 0, j = pixles.length; i < j; i += 4) {
+            var a = pixles[i + 3];
+            if (a ==0) {
+                transPixs.push(i);
+            }
+        }
+        var px=(transPixs.length / (pixles.length / 4) * 100).toFixed(2);
+        console.log(px);
+        if(px>50){
+            $("canvas").css("-webkit-transition","opacity 2s").css("opacity",0);
+            setTimeout(function(){
+                $("canvas").css("display","none");
+            },2000);
+        }
+        $(document).off();
     })
     e.stopPropagation();
 })
