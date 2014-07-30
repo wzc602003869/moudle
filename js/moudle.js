8b07dafdf74899ddf072f8baa7f6803872949a44
wzc602003869 <602003869@qq.com>
2014-07-30 08:27:57 +0800

    first

--- js/moudle.js
+++ js/moudle.js
@@ -2,10 +2,15 @@
  * Created by lenovo on 2014/7/30.
  */
 var height = document.documentElement.clientHeight,top1;
-alert(height);
+//alert(height);
 $(".page").on("touchstart",function(e){
-    console.log(e.touches);
+   // console.log(e.touches);
+    var i = $(this).index();
+    console.log(i);
     top1 = event.touches[0].clientY;
-    alert(top1);
-    $(document).on("touchmove")
+    $(document).on("touchmove",function(){
+        top2 = event.touches[0].clientY;
+        console.log(top1,top2);
+
+    })
 })
