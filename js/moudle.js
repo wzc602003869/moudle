477f8930635050f0a1bdf865fc7260dd3afcac9f
wzc602003869 <602003869@qq.com>
2014-08-02 17:56:32 +0800

    添加了图片飞入动画。

--- js/moudle.js
+++ js/moudle.js
@@ -1,16 +1,10 @@
-/**
- * Created by lenovo on 2014/7/30.
- */
 console.log($);
 document.addEventListener("touchmove", function(e){
     e.preventDefault();
 }, false);
 var height = document.documentElement.clientHeight,width = document.documentElement.clientWidth,top1;
-//alert(height);
 $(".page").on("touchstart",function(e){
-   // console.log(e.touches);
     var i = $(this).index();
-    //alert($(".page").length)
     var p = i-1;
     var q = i+1;
     if(i==0){
@@ -21,7 +15,6 @@ $(".page").on("touchstart",function(e){
     top1 = event.touches[0].clientY;
     $(document).on("touchmove",function(){
         top2 = event.touches[0].clientY;
-        //console.log(top1,top2);
         if(top2>top1){
             var y = -height+top2-top1;
             var s = 1-(top2-top1)/height*0.2;
@@ -32,16 +25,6 @@ $(".page").on("touchstart",function(e){
             sca = sca+str1;
             $(".page").eq(i).css("-webkit-transform",sca);
             $(".page").eq(p).addClass("move").css("-webkit-transform",str);
-//            $(document).on("touchend",function(){
-//                $(".page").eq(i-1).addClass("action").css("-webkit-transform","translate(0px,0px)");
-//                $(".page").eq(i).addClass("action").css("-webkit-transform","translate(0px,"+(height*0.2)+"px) scale(0.8)");
-//                setTimeout(function(){
-//                    $(".page").removeClass("action active move");
-//                    $(".page").eq(i-1).removeClass("action move").addClass("active");
-//
-//                },200);
-//                $(document).off();
-//            })
         }else if(top2<top1){
             var y = height+top2-top1;
             var s = 1-(top1-top2)/height*0.2;
@@ -53,17 +36,6 @@ $(".page").on("touchstart",function(e){
             sca = sca+str1;
             $(".page").eq(i).css("-webkit-transform",sca);
             $(".page").eq(q).addClass("move").css("-webkit-transform",str);
-//            $(document).on("touchend",function(){
-//                $(".page").eq(i+1).addClass("action").css("-webkit-transform","translate(0px,0px)");
-//                $(".page").eq(i).addClass("action").css("-webkit-transform","translate(0px,"+(-height*0.2)+"px) scale(0.8)");
-//                setTimeout(function(){
-//                    $(".page").removeClass("action active move");
-//                    $(".page").eq(i+1).removeClass("action move").addClass("active");
-//
-//                },200);
-//                $(document).off();
-//
-//            })
         }
         $(document).on("touchend",function(){
             if(top2<top1){
@@ -72,7 +44,6 @@ $(".page").on("touchstart",function(e){
                 setTimeout(function(){
                     $(".page").removeClass("action active move");
                     $(".page").eq(q).removeClass("action move").addClass("active");
-
                 },200);
                 $(document).off();
             }else{
@@ -81,7 +52,6 @@ $(".page").on("touchstart",function(e){
                 setTimeout(function(){
                     $(".page").removeClass("action active move");
                     $(".page").eq(p).removeClass("action move").addClass("active");
-
                 },200);
                 $(document).off();
             }
@@ -93,11 +63,9 @@ var mus= 0,time;
 $("#musicbtn").on("touchstart",function(e){
     clearTimeout(time);
     $(".musiccon").removeClass("animate");
-    //$(".musiccon").eq(mus).css("display","block").css("opacity",1);
     $(".musiccon").eq(mus).addClass("animate");
     time = setTimeout(function(){
         $(".musiccon").removeClass("animate");
-       // $(".musiccon").removeClass("show").animate({"opa"});
     },2001)
     if(mus==0){
         $("#bgmp3")[0].play();
